be8e2dfef3411bc199279e815f2f68a5263b2377
Salvador Chamizo <salvadorchamizo@gmail.com>
2025-10-24 23:39:27 +0200

    feat: Add basic setup with containers

---
+++ ex00/backend/src/index.ts
@@ -0,0 +1,11 @@
+import Fastify from "fastify";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = Fastify({ logger: true });
+
+const PORT = process.env.PORT || 8081;
+
+app.listen({ port: Number(PORT), host: "0.0.0.0" })
+    .then(() => console.log(`Backend-service listening at port ${PORT}`))
---
+++ ex00/frontend/src/main.ts
@@ -0,0 +1,8 @@
+function main() {
+    console.log("Entra");
+    const app = document.getElementById("app");
+    if (app)
+        app.innerHTML = "<div>Hello World</div>";
+}
+
+main();
